3fb35e9d96d66a96434c122bf697a057cb8b43e0
Moyo75 <moyojohn188@gmail.com>
2022-12-02 09:22:25 +0100

    endpoints for getting all articles and just one

--- server.js
+++ server.js
@@ -6,13 +6,42 @@ import { createClient } from "@supabase/supabase-js";
 env.config();
 
 const app = express();
-const router = express.Router;
 
-app.get("/", (request, response) =>
+const supabase = createClient(
+  process.env.DATABASE_URL,
+  process.env.DATABASE_KEY
+);
+
+app.get("/", (_, response) =>
   response.json({ info: "Node.js, Express, and Postgres API" })
 );
 
-app.get("/articles", getAllArticles);
+// Get all articles
+app.get("/articles", async (_, response) => {
+  try {
+    const { data, error } = await supabase.from("blog").select();
+    console.log(data);
 
-//console.log(process.env.PORT);
+    return response.send(data);
+  } catch (error) {
+    return response.send({ error });
+  }
+});
+
+// Get an article
+app.get("/articles/:id", async (request, response) => {
+  try {
+    const { data, error } = await supabase
+      .from("blog")
+      .select()
+      .eq("id", request.params.id)
+      .order("id", { ascending: true });
+
+    console.log(data);
+    return response.send(data);
+  } catch (error) {
+    return response.send({ error });
+  }
+});
+
 app.listen(process.env.PORT, () => console.log("Server is running..."));
