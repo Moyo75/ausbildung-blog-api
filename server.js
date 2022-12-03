4d926d6ecb781edcdcee883bdf56f7cf8eea60df
Moyo75 <moyojohn188@gmail.com>
2022-12-03 14:07:24 +0100

    post, update, delete endpoints

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from "express";
 //import { getAllArticles } from "./queries.js";
 import env from "dotenv";
+
+import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
 
 env.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const supabase = createClient(
   process.env.DATABASE_URL,
@@ -44,4 +50,58 @@ app.get("/articles/:id", async (request, response) => {
   }
 });
 
-app.listen(process.env.PORT, () => console.log("Server is running..."));
+// Post an article
+app.post("/articles", async (request, response) => {
+  try {
+    console.log(request.body);
+    const { data, error } = await supabase.from("blog").insert(request.body);
+    if (error) {
+      return response.status(400).json(error);
+    }
+    response.status(200).json(request.body);
+  } catch (error) {
+    response.send({ error });
+  }
+});
+
+// Update an article
+app.put("/articles/:id", async (request, response) => {
+  console.log(request.params);
+  try {
+    const { data, err } = await supabase
+      .from("blog")
+      .select()
+      .eq("id", parseInt(request.params.id));
+    const { data: updatedData, error: updatedError } = await supabase
+      .from("blog")
+      .update({
+        title: request.body.title ? request.body.title : data[0].title,
+        content: request.body.content ? request.body.content : data[0].content,
+        tags: request.body.tags ? request.body.tags : data[0].tags,
+      })
+      .eq("id", request.params.id);
+    response.send(request.body);
+  } catch (error) {
+    response.send({ error });
+  }
+});
+
+// Delete an article
+app.delete("/articles/:id", async (request, response) => {
+  try {
+    const { data, error } = await supabase
+      .from("blog")
+      .delete()
+      .eq("id", request.params.id);
+    if (error) {
+      return response.status(400).json(error);
+    }
+    response.status(200).json({ data });
+  } catch (error) {
+    response.send({ error });
+  }
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(new Date().toLocaleTimeString() + ": Server is running...")
+);
